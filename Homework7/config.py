32b67cd7215f3836ee743cd7ecf817fc5123da4e
jesseyu0323 <jesseyu.nwds@gmail.com>
2018-09-07 18:59:16 -0500

    homework7

---
+++ Homework7/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "sW1ab2ZejucwJaxalG9wHJg8i" 
+consumer_secret = "oefkmlxdesISijkOvNeZrcoSGjPOsY18fjLqNOMHCn4niEc5U0"
+access_token = "479595830-FIEK9gScPKGkyhTK6DMxy6QOIGjRxvPEICjRxQJc"
+access_token_secret = "WqFPKHICeyveM7xH7OdYjvozOLUt5ya4GElV7L8gCZyvJ"
